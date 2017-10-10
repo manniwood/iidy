56ab71b0b777e47936dace69d8725f57edb353e1
Manni Wood <manni.lee.wood@gmail.com>
2017-10-09 21:11:47 -0400

    Adds comment on env struct inspiration

--- handlers.go
+++ handlers.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+// The whole way of setting up these structs
+// and the handler so that I can provide access
+// to the Store is inspired by
+// https://elithrar.github.io/article/http-handler-error-handling-revisited/
+
 type Env struct {
 	Store Store
 }
@@ -18,8 +23,6 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.H(h.Env, w, r)
 }
 
-// OK, but how do I get a reference to an instance of
-// the datastore?
 func HelloWorldHandler(e *Env, w http.ResponseWriter, r *http.Request) {
 	// e now has our env
 	fmt.Fprint(w, "Hello World\n")
