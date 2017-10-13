24b648042000bec0a4e6e065041f9dcbf7a2943d
Manni Wood <manni.lee.wood@gmail.com>
2017-10-13 18:06:56 -0400

    Correctly uses handler

--- cmd/iidy/main.go
+++ cmd/iidy/main.go
@@ -11,10 +11,13 @@ import (
 func main() {
 	port := 8080
 
-	env := &iidy.Env{Store: iidy.NewMemStore()}
+	s, err := iidy.NewPgStore()
+	if err != nil {
+		log.Fatalf("Problem with store: %v\n", err)
+	}
+	env := &iidy.Env{Store: s}
 
-	http.Handle("/helloworld", iidy.Handler{Env: env, H: iidy.HelloWorldHandler})
-	// http.HandleFunc("/helloworld", iidy.HelloWorldHandler)
+	http.Handle("/lists/", iidy.Handler{Env: env, H: iidy.ListHandler})
 
 	log.Printf("Server starting on port %d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
