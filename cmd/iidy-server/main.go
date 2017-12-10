face7bc981bde701be89f077e8063969564d0d6a
Manni Wood <manni.lee.wood@gmail.com>
2017-12-10 16:33:13 -0500

    Adds put command

--- cmd/iidy-server/main.go
+++ cmd/iidy-server/main.go
@@ -11,20 +11,31 @@ import (
 )
 
 // TODO: presumably a reference to an iidy pg store would go in the server
-type server struct{}
+type server struct {
+	Store *iidy.PgStore
+}
 
 // Put implements iidy.RPCerServer
 func (s *server) Put(ctx context.Context, in *iidy.Entry) (*iidy.Reply, error) {
-	return &iidy.Reply{Verb: "PUT", Count: 1}, nil
+	count, err := s.Store.Add(in.List, in.Item)
+	if err != nil {
+		return nil, err
+	}
+	return &iidy.Reply{Verb: "ADDED", Count: count}, nil
 }
 
 func main() {
+	pgStore, err := iidy.NewPgStore()
+	if err != nil {
+		log.Fatalf("Problem with store: %v\n", err)
+	}
+
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	iidy.RegisterRPCerServer(s, &server{})
+	iidy.RegisterRPCerServer(s, &server{Store: pgStore})
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
